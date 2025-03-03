71d7309acaec8734a9b18ac5fdf7c6a46b5d3600
Muhammad Zafar <142920742+Zafar205@users.noreply.github.com>
2025-03-03 11:24:54 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,6 +117,21 @@ export default function Page() {
           </div>
         </div>
       </section>
+
+       <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold text-white">Languages and Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
       
        <section id="work">
         <div className="flex min-h-0 flex-col gap-y-2">
@@ -166,20 +181,7 @@ export default function Page() {
           ))}
         </div>
       </section> */}
-      <section id="skills">
-        <div className="flex min-h-0 flex-col gap-y-3">
-          <BlurFade delay={BLUR_FADE_DELAY * 9}>
-            <h2 className="text-xl font-bold text-white">Languages and Skills</h2>
-          </BlurFade>
-          <div className="flex flex-wrap gap-1">
-            {DATA.skills.map((skill, id) => (
-              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
-                <Badge key={skill}>{skill}</Badge>
-              </BlurFade>
-            ))}
-          </div>
-        </div>
-      </section>
+     
       {/* <section id="hackathons">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
